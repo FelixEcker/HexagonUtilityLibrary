664d34fa486a068d10c7a437aa45611ec5e37535
Bertrahm <felix_eckert_@t-online.de>
2020-07-09 18:45:40 +0200

    Added Minecraft Utilities that I forgot to commit

---
+++ src/de/hexagonstudios/hul/minecraft/EntityBiomeVariations.java
@@ -0,0 +1,68 @@
+package de.hexagonstudios.hul.minecraft;
+
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ * This class is used to store Variation Numbers,
+ * together with the appropriate Biome, to create
+ * a Map of Entity Variations according to their Biomes.
+ * 
+ * @author Felix Eckert
+ * */
+public class EntityBiomeVariations {
+	private HashMap<String, Integer[]> types;
+	
+	public EntityBiomeVariations() {
+		this.types = new HashMap<String, Integer[]>();
+	}
+	
+	/**
+	 * Used to add a Variation for a Biome.
+	 * 
+	 * @param biome The biome for which the variations should be added
+	 * @param vars An array of Integers containing the Entity Variant IDs.
+	 * 
+	 * @author Felix Eckert
+	 * */
+	public void addVariation(String biome, Integer[] vars) {
+		this.types.put(biome, vars);
+	}
+	
+	/**
+	 * Gets the According Entity Type IDs according to the
+	 * Biome given
+	 * 
+	 * @param biome The Biome the Entity Spawns in
+	 * @return -1 if the Biome isnt contained. Else it will return the according id
+	 * 
+	 * @author Felix Eckert
+	 * */
+	public Integer[] getVariationsOfBiome(String biome) {
+		if (this.types.containsKey(biome))
+			return (Integer[]) this.types.get(biome);
+		
+		return new Integer[] {-1};
+	}
+	
+	/**
+	 * Gets a random entity type id, from the given
+	 * biome.
+	 * 
+	 * @param biome The Biome the Entity Spawns in
+	 * @return -1 if the Biome isnt contained. Else it will return the random id
+	 * 
+	 * @author Felix Eckert
+	 */
+	public int getRandomVariationOfBiome(String biome) {
+		if (!this.types.containsKey(biome))
+			return -1;
+		
+		Random r = new Random();
+		int type;
+		Integer[] entityTypes = types.get(biome);
+		type = entityTypes[r.nextInt(entityTypes.length)];
+		
+		return type;
+	}
+}
---
+++ src/de/hexagonstudios/hul/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Hexagon Utility Library (HUL) v1.1.0
+ * Copyright (c) 2020 Felix Eckert.
+ * 
+ * This Library Contains several utilities
+ * for all kinds of stuff. You may extract
+ * only the parts you need and use it for free,
+ * regardless if the software is free or paid.
+ * 
+ * @author Felix Eckert
+ * @version 1.1.0
+ * */
+package de.hexagonstudios.hul;
